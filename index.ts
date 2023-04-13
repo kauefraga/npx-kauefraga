253aae4eed7bb47e7cb8143f574a09a4fc6c3512
Kauê Fraga Rodrigues <kauefragarodrigues456@gmail.com>
2023-04-13 19:05:50 -0300

    :sparkles: feat: greet and show my social medias with colors =)

---
+++ index.ts
@@ -0,0 +1,13 @@
+#!/usr/bin/env node
+
+import c from 'ansi-colors';
+
+console.log(
+  c.bold(`${c.magenta('Hii!')} I am Kauê and i like to work with the backend!`)
+);
+
+console.log(
+  c.cyan('\n> kauefragarodrigues456@gmail.com'),
+  c.cyan('\n> https://github.com/kauefraga'),
+  c.cyan('\n> https://www.npmjs.com/~kauefraga')
+)
